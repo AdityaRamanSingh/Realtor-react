c3c872da553c007f1b15557074987d8918b74f65
Aditya Raman Singh Panwar <panwaradityaramansingh@gmail.com>
2023-05-24 12:01:29 +0530

    Create components and their routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Home from "./components/Home"
+import Profile from "./components/Profile"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import ForgotPassword from "./components/ForgotPassword"
+import Offers from "./components/Offers"
+
+ 
 
 function App() {
   
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!</h1>
+  <>
+    <Router>
+      <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/profile" element={<Profile/>}/>
+     <Route path="/sign-in" element={<SignIn/>}/>
+     <Route path="/sign-up" element={<SignUp/>}/>
+     <Route path="/forgot-password" element={<ForgotPassword/>}/>
+     <Route path="/offers" element={<Offers/>}/>
+      
+      </Routes>
+    </Router>
     </>
   )
 }
